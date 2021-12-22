75dff889010a7700e877c094863fd3f61e169d04
salzanopm <salzanopm@gmail.com>
2021-12-22 16:47:49 +0100

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <img src="../assets/spotify.png" alt="logo spotify">
+    </header>
+</template>
+<script>
+
+export default {
+    name:'Header'
+}
+</script>
+
+<style scoped lang="scss">
+    img {
+        height: 100%;
+        
+    }
+
+    header {
+        height: 50px;
+        padding: 5px;
+        background-color: #2e3a46;
+    }
+</style>
